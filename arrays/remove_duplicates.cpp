e7db3db055c97da7208f94dc579367a261c832ce
benwilliamss <benwilliams1996@hotmail.co.uk>
2021-10-07 09:38:49 +0100

    Slightly cleaner version. Print_numbers functions to show result output to user.

--- arrays/remove_duplicates.cpp
+++ arrays/remove_duplicates.cpp
@@ -4,45 +4,53 @@
 
 using namespace std;
 
+void print_numbers(vector<int> nums)
+{	
+	printf("\n\nresult: ");
+	for(int &x: nums) printf("%d, ", x); 
+	printf("\n");
+	
+}
+
 
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) 
     {
-	printf("value at num[size]: %d", *(nums.end())); 
-	
-	 for(int i = 0 ; i < nums.size(); ++i)
-	{	
+	//condition that the nums vector contains numbers in non-decreasing order
 		
-		printf("current num: %d", nums[i]);	    
-	    	if(nums[i] == nums[i+1] && i+1 < nums.size())
-		{	printf(".. duplicate: %d", nums[i+1]);
-			nums.erase(nums.begin()+i); //erased at memory location
-			i--;
-				
-		}
-		printf("\n");
-	}
-	printf("\n");
-	for(int &x: nums)printf("%d, ",x);
+	for(int i = 0 ; i < nums.size(); ++i)
+	{	
+		if(i+1 == nums.size())break;//prevents index out of bounds
+		
 
-    	return nums.size();	    
+	  	printf("\ncurrent num %d" ,nums[i]);
+	    	if(nums[i] == nums[i+1])
+		{	printf(".. duplicate: %d\n", nums[i+1]);
+			nums.erase(nums.begin()+i);
+		       	i--; //reason: need to evaluate the current num until no duplicates							
+		}	
+	}
+	print_numbers(nums);	
+	return nums.size();
+    
         
     }
 };
+
 
 int main()
 {
 	//tests
 	Solution newSolution;
 	
-	vector<int> test = {0,0,1,1,1,2,2,3,3,4};
+	vector<int> test = {0,0,1,1,1,2,2,3,3,3,4,4};
 	newSolution.removeDuplicates(test);
 	printf("\n");	
 
 	
 
-	vector<int> test_two = {0,1,2,3,4,5,5,5,8,8};
+	vector<int> test_two = {0,0,1,8};
 	newSolution.removeDuplicates(test_two);
 
 }
